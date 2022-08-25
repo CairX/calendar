0075c074b03afc839a2533f51ab734184533f167
Thomas Cairns <thomas@cairns.se>
2022-08-25 16:01:31 +0200

    Follow ISO 8601 on calculating the week for a date
    The intention is to increase the accuracy of the week number calculation.

--- scripts/dateutils.js
+++ scripts/dateutils.js
@@ -34,6 +34,62 @@ var DateUtils = (function() {
 		{ 'month': 5, 'start': 20, 'end': 26, 'name': 'Alla helgons dag' }
 	];
 
+	var monthOrdinalDayCommonYear = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+
+	var isLeapYear = function(year) {
+		if (year % 4 != 0)
+			return false;
+		if (year % 100 != 0)
+			return true;
+		if (year % 400 != 0)
+			return false;
+		return true;
+	};
+	var isCommonYear = function(year) {
+		return !isLeapYear(year);
+	};
+
+	var getOrdinalDay = function(date) {
+		var month = date.getMonth();
+		var ordinal = monthOrdinalDayCommonYear[month] + date.getDate();
+		// Early exist when we know that leap year calculation isn't needed.
+		if (month < 2)
+			return ordinal;
+		var leapYear = isLeapYear(date.getFullYear());
+		return ordinal + (leapYear ? 1 : 0);
+	};
+
+	/* ISO 8601 - Weeks per year
+	 * The long years, with 53 weeks in them, can be described by any of
+	 * the following equivalent definitions:
+	 * - any year starting on Thursday (dominical letter D or DC) and any
+	 *   leap year starting on Wednesday (ED)
+	 * - any year ending on Thursday (D, ED) and any leap year ending on Friday (DC)
+	 * - years in which 1 January or 31 December are Thursdays
+	 */
+	var getWeeks = function(year) {
+		var date = new Date(year, 0, 4);
+		if (date.getDay() == 4)
+			return 53;
+		date = new Date(year - 1, 11, 31);
+		if (date.getDay() == 3)
+			return 53;
+		return 52;
+	};
+
+	var getWeek = function(date) {
+		var year = date.getFullYear();
+		var ordinal = getOrdinalDay(date);
+		var dayOfTheWeek = (date.getDay() + 1 % 7) + 1;
+		var week = Math.floor((10 + ordinal - dayOfTheWeek) / 7);
+
+		if (week < 1)
+			return getWeeks(year - 1);
+		if (week > getWeeks(year))
+			return 1;
+		return week;
+	};
+
 	var addRedDay = function(date) {
 		for (var i = 0; i < redDays.length; i++) {
 			var red = redDays [i];
@@ -93,20 +149,6 @@ var DateUtils = (function() {
 			date.setDate(date.getDate() + 1);
 		}
 		return dates;
-	};
-
-	var getWeek = function(date) {
-		var firstDayOfTheYear   = new Date(date.getFullYear(), 0);
-		var isFirstDayInWeekOne = ConvertToSwedishDay(firstDayOfTheYear.getDay()) <= 3;
-
-		var dayOfWeekInMS    = ConvertToSwedishDay(date.getDay()) * dayInMilliseconds;
-		var mondayOfWeekInMS = date.getTime() - dayOfWeekInMS - firstDayOfTheYear.getTime();
-
-		var weekThisYear = Math.ceil(mondayOfWeekInMS / weekInMilliseconds);
-		weekThisYear     = weekThisYear + (isFirstDayInWeekOne ? 1 : 0);
-		weekThisYear     = weekThisYear == 0 ? 53 : weekThisYear
-
-		return weekThisYear;
 	};
 
 	var padding = function(number) {
